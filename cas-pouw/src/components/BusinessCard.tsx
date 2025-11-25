df5ddbd709e933a3a7b2b9a4014d6ebc6ade82c7
Cas Pouw <c.a.s.pouw@tue.nl>
2025-11-25 15:46:57 +0100

    Simple react business card

---
+++ cas-pouw/src/components/BusinessCard.tsx
@@ -0,0 +1,48 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import ContactInfo from "./ContactInfo";
+import { Mail, Business } from "@mui/icons-material";
+
+export default function BusinessCard() {
+  const name = "Cas Pouw";
+  const title = "Crowdflow specialist";
+  const description =
+    "I’m speicalized in understanding and optimizing crowd environments. If you’re facing a challenge related to pedestrian flows, I’d love to explore how we can solve it together.";
+  const myEmail = "cas.pouw@proton.me";
+  const myBusiness = "In development";
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h3"
+          component="div"
+          sx={{ textAlign: "left" }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ textAlign: "left" }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", textAlign: "left", marginBottom: 2 }}
+        >
+          {description}
+        </Typography>
+        <ContactInfo title={myEmail} subheader="Email" icon={<Mail />} />
+        <ContactInfo
+          title={myBusiness}
+          subheader="Business"
+          icon={<Business />}
+        />
+      </CardContent>
+    </Card>
+  );
+}
---
+++ cas-pouw/src/components/ContactInfo.tsx
@@ -0,0 +1,22 @@
+import CardHeader from "@mui/material/CardHeader";
+
+interface ContactInfoProps {
+  title: string;
+  subheader: string;
+  icon?: React.ReactNode;
+}
+
+export default function ContactInfo({
+  title,
+  subheader,
+  icon,
+}: ContactInfoProps) {
+  return (
+    <CardHeader
+      avatar={icon}
+      title={title}
+      subheader={subheader}
+      sx={{ textAlign: "left" }}
+    />
+  );
+}
